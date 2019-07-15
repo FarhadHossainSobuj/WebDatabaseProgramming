84dc3d62c67a2b86e562e7431e7027026cb4f49b
Farhad Hossain <32379259+FarhadHossainSobuj@users.noreply.github.com>
2019-07-16 00:23:59 +0600

    Update index.php

--- Homework7/index.php
+++ Homework7/index.php
@@ -5,12 +5,12 @@
     <meta charset="UTF-8">
     <title>Home</title>
     
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     
 
-    <script src="../js/jquery.js"></script>
+    <script src="js/jquery.js"></script>
 
-    <script src="../js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
     
     <link rel="stylesheet" href="style.css">
 </head>
